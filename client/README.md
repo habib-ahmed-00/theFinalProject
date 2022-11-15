ed575c55830dbb6a50393950a412b25d053f873c
habib-ahmed-00 <hahmed.y2k@gmail.com>
2022-11-15 23:44:54 +0530

    Changed Logo and flag logo

--- client/README.md
+++ client/README.md
@@ -1,6 +1,6 @@
 # WEB3 - Ecommerce
 
-## A Moralis based ecommerce application. Full fledged ecom flow and user interaction is saved and recprded on the blockchain.
+## A Moralis based ecommerce application. Full fledged ecom flow and user interaction is saved and recorded on the blockchain.
 
 - yarn
 - yarn start
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import { useMoralis } from "react-moralis";
 import { Link } from 'react-router-dom';
 import './Header.css'
 import Amazon from "../images/logo.png";
-import USA from "../images/usa.png";
+import USA from "../images/ind.png";
 import BookStore from "../images/bookstore.png";
 import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
 
